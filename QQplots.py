e71a1c1137884e7c4fe97f0246c2fcbd2dd605cb
chdamianos <chdamianos@gmail.com>
2019-01-06 20:07:21 +0000

    minor variable naming change

--- QQplots.py
+++ QQplots.py
@@ -10,18 +10,18 @@ quantilesSample = np.arange(0, sample.shape[0]) / (sample.shape[0] - 1)
 # get quantiles of any distribution we are interested in
 quantileArea = 1. / (sample.shape[0] + 1)
 quantiles = np.cumsum(np.ones(sample.shape[0]) * quantileArea)
-quantiles = norm.ppf(quantiles, loc=0, scale=1)
+quantilesLoc = norm.ppf(quantiles, loc=0, scale=1)
 # compare distribution quantiles with data
 # sum of squared errors of the model
-sse = np.sum(np.square(np.sort(sample) - quantiles))
+sse = np.sum(np.square(np.sort(sample) - quantilesLoc))
 # total of squared errors of the model
 sst = np.sum(np.square(np.sort(sample) - np.mean(np.sort(sample))))
 # coefficient of determination
 r2 = 1. - (sse / sst)
 # plot
 figQQ, axQQ = plt.subplots()
-axQQ.scatter(x=quantiles, y=np.sort(sample), label='Sample')
-axQQ.plot(quantiles, quantiles, label='Theoretically expected', color='red')
+axQQ.scatter(x=quantilesLoc, y=np.sort(sample), label='Sample')
+axQQ.plot(quantilesLoc, quantilesLoc, label='Theoretically expected', color='red')
 axQQ.set_xlabel('Theoretical Quantiles')
 axQQ.set_ylabel('Sample Quantiles')
 axQQ.legend()
